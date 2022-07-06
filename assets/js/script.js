e0b1a355e372c7fb984420603d9619b1ea250fcf
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-07-05 22:22:54 -0500

    Formatted the times displayed and deleted unnecessary comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,13 @@
-// var previousCities = ["Houston","Austin","New York","Dallas"];
-
 // the API offers One Call API 1.0 that allows the user to fetch once and the response return all the current, hourly, daily data at once.
 // but it only allows lat,long as query parameters- so I use the current weather api call so I can search by city name then pass the returned lat/long to One Call API 1.0
 var getLatLong = function(event) { 
-    // event.preventDefault();
 
-    // grabs the city from the input
-    var cityName = $('input')[0].value;
-    // if Search! button is hit but no text is inside the input...
+    var cityName = $('input')[0].value.trim().toUpperCase();
+
     if (!cityName) {
         alert("You need to search for a city first!");
         return;
     }
-    
-    buttonSave(cityName);
 
     apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=8750441917e6ca1d96a12baea082a59e";
 
@@ -24,39 +18,46 @@ var getLatLong = function(event) {
                     var cityLong = data.coord.lon;
                     var cityLat = data.coord.lat;
                     oneCall(cityLat,cityLong);
+                    buttonSave(cityName);
                 });
             } else {
                 alert("Error. The city was not found. Please check your spelling and try again.");
-            }
+            };
+        })
+        .catch(function(error) {
+            alert("Unable to connect to API.");
         });
 };
 
+// eventListener keypress to validate "enter"
 var enterButton = function(event) {
     if (event.which == 13) {
         event.preventDefault();
         getLatLong();
-    }
+    };
 };
 
 // uses latitude and longitudes to use One Call API 1.0 for complete weather information
 var oneCall = function(latitude,longitude) {
+
     apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=8750441917e6ca1d96a12baea082a59e";
-    
+
     fetch(apiUrl)
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function(data) {
-                    console.log(data);
                     currentWeather(data);
                     forecastWeather(data);
                 });
             } else {
                 alert("Error.");
             }
+        })
+        .catch(function(error) {
+            alert("Cannot connect to API.");
         });
-}
+};
 
-// populates the current weather section
 var currentWeather = function(data) {
     $('#currentWeather').empty();
     $('#currentWeather').addClass('parent border');
@@ -66,11 +67,9 @@ var currentWeather = function(data) {
     var currentWindSpeed = data.current.wind_speed;
     var currentUVI = data.current.uvi;
 
-    var cityName = $('input')[0].value;
-    // var cityEl = $("<h2>" + cityName + "</h2>");
+    var cityName = $('input')[0].value.trim().toUpperCase();
     var cityEl = $("<h2>");
     $(cityEl).text(cityName);
-    // style City Name here and write the city name in Title Case somehow
     $('#currentWeather').append(cityEl);
 
     var currentImg = $('<img>').attr('src',"http://openweathermap.org/img/w/" + currentIcon + ".png");
@@ -85,6 +84,7 @@ var currentWeather = function(data) {
     });
     var timeEl = $("<span>");
     timeEl.text(currentDate);
+    timeEl.addClass('time');
     $('#currentWeather').append(timeEl);
 
     var currentList = $('<ul>');
@@ -136,11 +136,10 @@ var currentWeather = function(data) {
     currentList.append(currentListWind);
 };
 
-// populates the five day forecast section
 var forecastWeather = function(data) {
     $('#forecastWeather').empty();
     $('#forecastWeather').addClass('parent border');
-    $('#forecastWeather').append($('<h4>').attr('class','cell small-12 forecast parent').text("Five Day Forecast:"));
+    $('#forecastWeather').append($('<h4>').attr('class','cell small-12 parent').text("Five Day Forecast:"));
 
 
     for (var i = 0; i < 5; i++) {
@@ -162,13 +161,14 @@ var forecastWeather = function(data) {
 
         var forecastList = $('<ul>');
         forecastList.attr('style','list-style:none');
-        forecastList.attr('class','cell small-12 medium-5 large-2 parent border');
+        forecastList.attr('class','cell small-12 medium-5 large-2 parent forecast');
         $("#forecastWeather").append(forecastList);
 
         forecastList.append(forecastImg);
 
         var forcastDateEl = $('<li>')
         forcastDateEl.text(formatDate);
+        forcastDateEl.addClass('forecast-time');
         forecastList.append(forcastDateEl);
 
         var forecastTempLiEl = $('<li>');
@@ -184,11 +184,12 @@ var forecastWeather = function(data) {
         forecastList.append(forecastHumidLiEl);
         forecastList.append(forecastWindLiEl);
     };
-}
+    $('input').val("");
+};
 
 var buttonSave = function(cityName) {
     previousCities = JSON.parse(localStorage.getItem("Cities"));
-    // if my variable doesn't exist in localStorage, create one...
+
     if (!previousCities) {
         previousCities = [];
         previousCities.push(cityName);
@@ -196,7 +197,6 @@ var buttonSave = function(cityName) {
         var filteredCities = storageCheck(previousCities,cityName);
         for (var i = 0; i < filteredCities.length; i++) {
             if (filteredCities[i] === true) {
-                // make the button remove and then repopulate at the end
                 var match = true;
                 return match;
             };
@@ -227,7 +227,7 @@ var populateButtons = function() {
 var storageCheck = function(array,query) {
     var filteredCities = [];
     array.filter(function(array) {
-        filteredCities.push(array.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+        filteredCities.push(array.indexOf(query) !== -1);
         return filteredCities;
     });
     return filteredCities;
@@ -239,11 +239,10 @@ var recallWeather = function(event) {
     getLatLong();
 }
 
-// event "click" for Search Button
-$(".searchBtn").click(getLatLong);
-// event "keypress" for Enter to toggle the Search Button
-$("#cityName").keypress(enterButton);
-// populate localStorage buttons from previous searches at page load
 populateButtons();
-// event delegation
+
+$(".searchBtn").click(getLatLong);
+
+$("#cityName").keypress(enterButton);
+
 $('#buttonList').on("click",'.historyBtn', recallWeather);
